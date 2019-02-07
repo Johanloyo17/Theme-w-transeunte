aaa356618ba7ea10cbfddaecca9b879493eeec10
Ali Padron <alijesus29@gmail.com>
2019-02-07 19:10:30 -0400

    Post Type contenido agregado y custom query para front-page

--- functions.php
+++ functions.php
@@ -23,12 +23,32 @@ function transeunte_post_types() {
     'public' => true,
     'labels' => array(
       'name' => 'Eventos',
+      'add_new' => 'Añadir nuevo',
       'add_new_item' => 'Añadir nuevo',
       'edit_item' => 'Editar evento',
       'all_items' => 'Todos los eventos',
       'singular_name' => 'Evento'
     ),
     'menu_icon' => 'dashicons-calendar'
+  ));
+
+  // Contenido Post Type
+  register_post_type('contenido', array(
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array(
+      'slug' => 'contenidos'
+    ),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Contenidos',
+      'add_new' => 'Añadir nuevo',
+      'add_new_item' => 'Añadir nuevo',
+      'edit_item' => 'Editar contenido',
+      'all_items' => 'Todos los contenidos',
+      'singular_name' => 'Contenido'
+    ),
+    'menu_icon' => 'dashicons-editor-quote'
   ));
 }
 
