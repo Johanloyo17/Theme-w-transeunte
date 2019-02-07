c1a2be3055ef110cbef5779bf2319a7742eb7d43
Ali Padron <alijesus29@gmail.com>
2019-02-07 07:58:43 -0400

    Gulp agregado

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  cleanCSS = require('gulp-clean-css'),
+  themeLocation = './';
+
+gulp.task('minify-js', function (done) {
+  gulp.src([
+    themeLocation + 'js/wow.min.js', 
+    themeLocation + 'js/jquery-3.3.1.min.js', 
+    themeLocation + 'js/all.min.js', 
+    themeLocation + 'js/funciones.js', 
+    themeLocation + 'js/popper.min.js',
+    themeLocation + 'js/bootstrap.min.js',    
+  ])
+    .pipe(concat('main-bundle.js'))
+    // .pipe(uglify())
+    .pipe(gulp.dest(themeLocation + 'js/'))
+  done();
+});
+gulp.task('minify-css', function (done) {
+  gulp.src(themeLocation + 'css/*.css')
+    .pipe(concat('main-bundle.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(themeLocation + 'css/'))
+  done();
+});
+gulp.task('optimiza', gulp.series('minify-js', 'minify-css', function (done) {
+  console.log('Eres tu alicate!!!');
+  done();
+}));
